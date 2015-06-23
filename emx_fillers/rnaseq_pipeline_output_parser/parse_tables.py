6a0f5c954270b1a11a2e01280ea8761cdd75c675
npklein <niekdeklein@gmail.com>
2015-06-23 23:25:38 +0200

    keep _ from column name, and switch . to _

--- emx_fillers/rnaseq_pipeline_output_parser/parse_tables.py
+++ emx_fillers/rnaseq_pipeline_output_parser/parse_tables.py
@@ -6,12 +6,13 @@ Created on Jun 23, 2015
 
 def column_names_to_attribute_names(column_names):
     delchars = ''.join(c for c in map(chr, range(256)) if not c.isalnum())
+    delchars = delchars.translate(None,'._')
     attribute_names = []
     for column_name in column_names:
         # hacky way to preserve spaces
         column_name = column_name.replace(' ', 'SPAAAAAAAAAACE')
         column_name_clean = column_name.translate(None, delchars)
-        column_name_clean = column_name_clean.replace('SPAAAAAAAAAACE','_')
+        column_name_clean = column_name_clean.replace('SPAAAAAAAAAACE','_').replace('.','_')
         column_name_clean_list = list(column_name_clean)
         column_name_clean_list[0] = column_name_clean_list[0].lower()
         for index, letter in enumerate(column_name_clean_list[:-1]):
