4297a203c8adeb970ab7df2c00a1dfbe582a6e6a
npklein <niekdeklein@gmail.com>
2015-06-23 13:27:50 +0200

    initialize package

---
+++ emx_fillers/setup.py
@@ -0,0 +1,14 @@
+'''
+Created on Jun 23, 2015
+
+@author: Niek de Klein
+'''
+
+from setuptools import setup
+
+setup(name='emx_fillers',
+      version='0.1',
+      description='Parsing output data from rnaseq pipeline to put in .emx file',
+      url='http://github.com/npklein/emx_fillers/',
+      author='Niek de Klein',
+      packages=['rnaseq_pipeline_output_parser'])
